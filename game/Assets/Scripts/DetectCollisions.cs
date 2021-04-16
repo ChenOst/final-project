3e310c38412bca37a7e070e53fe439c7ed85b2c6
Chen Ostrovski <chenostrovski@gmail.com>
2021-04-16 20:55:10 +0300

    Created new script for DontDestroyOnLoad

--- game/Assets/Scripts/DetectCollisions.cs
+++ game/Assets/Scripts/DetectCollisions.cs
@@ -10,12 +10,6 @@ public class DetectCollisions : MonoBehaviour
     [SerializeField]
     private bool _active;
 
-    private GameObject _canvas;
-
-    private GameObject _manager;
-
-    private GameObject _ePanel;
-
     [SerializeField]
     [Tooltip("Number of Scene you would like to move to")]
     [Range(0,9)]
@@ -26,17 +20,14 @@ public class DetectCollisions : MonoBehaviour
     [Range(0, 9)]
     private int _controllerNumber;
 
-    private const string _managerName = "Game Manager";
-
-    private const string _canvasName = "Canvas";
+    private GameObject _manager, _ePanel;
 
     private bool _showPanel = false;
 
     void Start()
     {
-        _manager = GameObject.Find(_managerName);
-        _canvas = GameObject.Find(_canvasName);
-        _ePanel = _canvas.transform.FindChild("Press E Panel").gameObject;
+        _manager = GameObject.Find("Game Manager");
+        _ePanel = GameObject.Find("Canvas").transform.Find("Press E Panel").gameObject;
     }
 
     void Update()
@@ -45,18 +36,12 @@ public class DetectCollisions : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.E))
             {
-                GameObject sceneController = _manager.transform.GetChild(_controllerNumber).gameObject;
-                sceneController.SetActive(_active);
-
-                // Move Objects to the another scenes
-                DontDestroyOnLoad(_manager);
-                DontDestroyOnLoad(_canvas);
-                DontDestroyOnLoad(GameObject.Find("Main Camera"));
-                DontDestroyOnLoad(GameObject.Find("Prefab_PlayerCharacter"));
-                SceneManager.LoadScene(_sceneNumber);
-
                 _showPanel = false;
                 _ePanel.SetActive(false);
+
+                GameObject sceneController = _manager.transform.GetChild(_controllerNumber).gameObject;
+                sceneController.SetActive(_active);
+                SceneManager.LoadScene(_sceneNumber);
             }
         }
 
---
+++ game/Assets/Scripts/DontDestroyObjects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyObjects : MonoBehaviour
+{
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(GameObject.Find("Canvas"));
+        DontDestroyOnLoad(GameObject.Find("Main Camera"));
+        DontDestroyOnLoad(GameObject.Find("Prefab_PlayerCharacter"));
+    }
+}
